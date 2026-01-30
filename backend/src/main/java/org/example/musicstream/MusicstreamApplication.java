15f6abafe63bdf89046294bfc0b0e72205ecb843
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-01-30 17:04:11 +0100

    Add MusicstreamApplication main class

---
+++ backend/src/main/java/org/example/musicstream/MusicstreamApplication.java
@@ -0,0 +1,13 @@
+package org.example.musicstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicstreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicstreamApplication.class, args);
+    }
+
+}
