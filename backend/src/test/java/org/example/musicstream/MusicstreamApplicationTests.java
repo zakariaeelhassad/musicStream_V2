77a07657abc367026a84c16941798d8edf767d9b
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-01-30 17:05:38 +0100

    Add test files

---
+++ backend/src/test/java/org/example/musicstream/MusicstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.musicstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicstreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
