bb84de6cae83f0b25fbdc5cb9767aff318754f90
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-02-02 16:21:50 +0100

    feat(backend): add filePath field to TrackResponseDTO

--- backend/src/main/java/org/example/musicstream/dto/TrackResponseDTO.java
+++ backend/src/main/java/org/example/musicstream/dto/TrackResponseDTO.java
@@ -4,13 +4,15 @@ import java.time.LocalDateTime;
 
 public record TrackResponseDTO(
 
-        Long id,
-        String title,
-        String artist,
-        String description,
-        Long duration,
-        String category,
-        String fileUrl,
-        LocalDateTime createdAt
+                Long id,
+                String title,
+                String artist,
+                String description,
+                Long duration,
+                String category,
+                String fileUrl,
+                String filePath,
+                LocalDateTime createdAt
 
-) {}
+) {
+}
